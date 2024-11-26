bd4faa487b387133b35699b8aeb69c693ae11e2b
Vepari <veparisafin226@gmail.com>
2024-11-26 16:09:47 +0530

    date

--- TradingERP/Controllers/ReportsController.cs
+++ TradingERP/Controllers/ReportsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 using TradingERP.Services;
 
 namespace TradingERP.Controllers
@@ -42,6 +44,17 @@ namespace TradingERP.Controllers
                 {
                     return RedirectToAction("Index", "Home");
                 }
+                DateTime fDate,tDate;
+                if (DateTime.TryParseExact(fromDate, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out fDate))
+                {
+                    // Successfully parsed the date
+                    Console.WriteLine(fDate.ToString());
+                }
+                if (DateTime.TryParseExact(toDate, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out tDate))
+                {
+                    // Successfully parsed the date
+                    Console.WriteLine(tDate.ToString());
+                }
                 var userId = Request.Cookies["admToken"];
                 var userInfo = _userService.GetById(userId);
                 ViewData["userData"] = userInfo;
@@ -49,14 +62,14 @@ namespace TradingERP.Controllers
                 ViewBag.party = party;
                 if (fromDate != null)
                 {
-                    ViewBag.fromDate = Convert.ToDateTime(fromDate).ToString("dd-MM-yyyy");
+                    ViewBag.fromDate = fDate.ToString("dd-MM-yyyy");
                 }
                 if (toDate != null)
                 {
                     ViewBag.toDate = Convert.ToDateTime(toDate).ToString("dd-MM-yyyy");
                 }
 
-                var data = _reportService.RgReportByParty(userId, party, fromDate, toDate);
+                var data = _reportService.RgReportByParty(userId, party, fDate.ToString("dd-MM-yyyy"), tDate.ToString("dd-MM-yyyy"));
                 return View(data);
             }
             catch (Exception ex)
